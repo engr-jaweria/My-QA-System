aa380e48d5e25bb81eabfe4f1a9fd1c5b324f070
engr-jaweria <jzafar.msee23seecs@seecs.edu.pk>
2024-12-18 13:33:32 +0500

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -1,8 +1,7 @@
 import os
-
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_community.document_loaders import UnstructuredPDFLoader
+import pdfplumber
 from langchain_text_splitters.character import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -11,15 +10,19 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 
 
-# load the environment variables
+# Load the environment variables
 load_dotenv()
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def load_document(file_path):
-    loader = UnstructuredPDFLoader(file_path)
-    documents = loader.load()
+    documents = []
+    with pdfplumber.open(file_path) as pdf:
+        for page in pdf.pages:
+            text = page.extract_text()
+            if text:
+                documents.append(text)
     return documents
 
 
@@ -56,6 +59,7 @@ def create_chain(vectorstore):
     )
     return chain
 
+# Streamlit app setup
 st.set_page_config(
     page_title="Chat with Doc",
     page_icon="📄",
@@ -64,18 +68,16 @@ st.set_page_config(
 
 st.title("🦙 Chat with Doc - LLAMA 3.1")
 
-# initialize the chat history in streamlit session state
+# Initialize chat history in Streamlit session state
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
-
-uploaded_file = st.file_uploader(label="Upload your pdf file", type=["pdf"])
+uploaded_file = st.file_uploader(label="Upload your PDF file", type=["pdf"])
 
 if uploaded_file:
     file_path = f"{working_dir}/{uploaded_file.name}"
     with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
-
 
     if "vectorstore" not in st.session_state:
         st.session_state.vectorstore = setup_vectorstore(load_document(file_path))
@@ -83,13 +85,13 @@ if uploaded_file:
     if "conversation_chain" not in st.session_state:
         st.session_state.conversation_chain = create_chain(st.session_state.vectorstore)
 
+# Display chat history
 for message in st.session_state.chat_history:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-
+# User input for asking questions
 user_input = st.chat_input("Ask Llama...")
-
 
 if user_input:
     st.session_state.chat_history.append({"role": "user", "content": user_input})
@@ -97,11 +99,8 @@ if user_input:
     with st.chat_message("user"):
         st.markdown(user_input)
 
-
     with st.chat_message("assistant"):
         response = st.session_state.conversation_chain({"question": user_input})
         assistant_response = response["answer"]
         st.markdown(assistant_response)
         st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
-
-
